f10829283bed9c4e059d5a39b77b56289277e6ae
David Pascoal <david.pascoal@mercedes-benz.io>
2018-04-01 11:08:47 +0100

    fixed the service worker, it now responds correctly and stores the image assets

--- sw.js
+++ sw.js
@@ -1,24 +1,36 @@
+const staticCacheName = 'restaurant-static-v1'
+
 self.addEventListener('install', (event) => {
   const urlsToCache = [
     '/',
     'main.js',
     'data/restaurants.json',
-    'img/',
     'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2'
   ]
 
   event.waitUntil(
-    caches.open('restaurants-v1').then((cache) => {
+    caches.open(staticCacheName).then((cache) => {
       return cache.addAll(urlsToCache)
     })
   )
 })
 
 self.addEventListener('fetch', (event) => {
-  caches.match(event.request).then(response => {
-    console.log(response)
-    return response || fetch(event.request)
-  }).catch(error => {
-    console.log('error matching:', error)
-  })
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+        .then(response => {
+          // if it's a picture, we cache it
+          const imgData = response.clone()
+          if (imgData.url.match(/.*\.jpg/)) {
+            caches.open(staticCacheName).then((cache) => {
+              return cache.put(event.request, imgData)
+            })
+          }
+          return response
+        })
+    }).catch(error => {
+      console.log('error matching:', error)
+    })
+  )
 })
